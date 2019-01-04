d32b29e3a58f579cf02535726e9c9f33a02399d1
ThisIsNsh <hadanis.singh@gmail.com>
2019-01-04 22:14:14 +0530

    Status Bar

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/online/search/SearchFragment.java
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/online/search/SearchFragment.java
@@ -13,6 +13,7 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
